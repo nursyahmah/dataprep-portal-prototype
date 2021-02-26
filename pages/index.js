3c462470ba2d871afb9480c4310c0b35f445a846
nursyahmah <nsyj96@gmail.com>
2021-02-26 11:08:42 +0800

    fix changes

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid p-0">
         <a className="navbar-brand">
-          <img src="/logo-dataprepholdings.png" alt="dataprep logo" height="50"/>
+          <img src="/logo-dataprepholdings.png" alt="dataprep logo" height="45"/>
         </a>
         <a className="navbar-brand" href="#">
           HOME
@@ -47,23 +47,23 @@ return (
       </div>
     </nav>
     <div className="align-items-center" style={{background:"#e3e3e3", display:"flex"}}>
-        <div className="col-6" style={{paddingLeft:"90px"}}>
-            <h1 className="welcome">WELCOME To </h1>
-            <h1 className="mb-4 welcome">Dataprep Bhd</h1>
-            <p style={{fontSize:24, fontWeight:300}}>Dataprep Group offers Consulting & Information Technology Services to governments & industries including telecommunications, financial services, products & SMEs.</p>
-            <a href="" className="btn btn-lg btn-dark mt-4">LEARN MORE</a>
+        <div className="col-6" style={{paddingLeft:"80px"}}>
+            <h2 className="welcome">WELCOME To </h2>
+            <h2 className="mb-4 welcome">Dataprep Bhd</h2>
+            <p style={{fontSize:20, fontWeight:300}}>Dataprep Group offers Consulting & Information Technology Services to governments & industries including telecommunications, financial services, products & SMEs.</p>
+            <a href="" className="btn btn-lg btn-dark mt-2">LEARN MORE</a>
         </div>
         <img src="/images/server-room.jpg" alt="server-room" className="col p-0"/>
     </div>
-    <div className="row align-items-center" style={{height:"150px"}}>
-      <center style={{fontSize:30,fontWeight:"bold"}}>"CUSTOMER   SATISFACTION   IN   EVERY   DECISION"</center>
-    </div>
-    <hr/>
-    <div className="row">
-      <h5 className="pt-3 pb-3" style={{paddingLeft:"90px"}}>IT SOLUTIONS & SERVICES</h5>
+    <div className="row align-items-center" style={{height:"140px"}}>
+      <center style={{fontSize:20,fontWeight:"bold"}}>"CUSTOMER   SATISFACTION   IN   EVERY   DECISION"</center>
     </div>
 
-    <div className="row" style={{paddingLeft:"90px"}}>
+    <div className="row">
+      <h5 className="pt-3 pb-3" style={{paddingLeft:"80px"}}>IT SOLUTIONS & SERVICES</h5>
+    </div>
+
+    <div className="row" style={{paddingLeft:"80px"}}>
       <div className="card p-0" style={{width: "15rem"}}>
         <div className="card-img-top d-flex justify-content-center pb-4">
           <i className="fas fa-server fa-3x"></i>
@@ -76,7 +76,7 @@ return (
     </div>
     <style jsx>{`
     .welcome{
-      font-size: 50px;
+      font-size: 40px;
     }
 
     `}</style>
@@ -94,16 +94,16 @@ return (
       * {
         box-sizing: border-box;
       }
-      a{
-        font-size: 16px!important;
-        font-weight: 300;
-      }
       .active{
         background-color:#000;
         color:white;
       }
       .navbar{
         background-color: #FFF;
+      }
+      .navbar a{
+        font-size: 14px!important;
+        font-weight: 300;
       }
     `}</style>
   </>
