f917f6e02d524fef48e7b94701201da7ae330e9c
nursyahmah <33740877+nursyahmah@users.noreply.github.com>
2022-04-17 14:07:52 +0800

    revert

--- components/BrowserTopMenu.js
+++ components/BrowserTopMenu.js
@@ -3,38 +3,21 @@ import React from 'react'
 const BrowserTopMenu = () => {
     return (
 <>
-    <nav className="navbar pt-0" style={{ position:"sticky", top: 0, zIndex:"999", background:"#674ea7" }}>
+    <nav className="navbar" style={{ position:"sticky", top: 0, zIndex:"999", background:"#674ea7" }}>
           {/* Menu */}
-        <div className="p-0" style={{ paddingLeft: "30px" }}>
-          {/* <a className="navbar-brand">
-            <img src="/logo-dataprepholdings.png" alt="dataprep logo" height="45" />
-          </a> */}
-          <a className="navbar-brand mr-0" href="/"> Insights Portal |</a>
-          <a className="navbar-brand mr-0" href="/"> Merchant Center |</a>
-          <a className="navbar-brand mr-0" href=""> Blog |</a>
-          <a className="navbar-brand mr-0" href="/about_us"> About Us |</a>
-          <a className="navbar-brand mr-0" href="#"> FAQ </a>
-        </div>
         <div className="container-fluid p-0">
           <a className="navbar-brand">
             <img src="/logo-dataprepholdings.png" alt="dataprep logo" height="45" />
           </a>
-          <a className="navbar-brand" href="/"> Browse by Category </a>
-          <input/>
-          <a className="navbar-brand" href=""> 100VPoints </a>
-          <a className="btn btn-sm" href="#"> Earn More </a>
-          <div className="dropdown">
-          <button className="dropbtn">Dropdown</button>
-            <div className="dropdown-content">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-            </div>
-        </div>
+          <a className="navbar-brand" href="/"> HOME </a>
+          <a className="navbar-brand" href="/about_us"> ABOUT US </a>
+          <a className="navbar-brand" href="/investor_relations"> INVESTOR RELATIONS </a>
+          <a className="navbar-brand" href="#"> OUR SOLUTIONS </a>
+          <a className="navbar-brand" href="#"> PRESS ROOM </a>
+          <a className="navbar-brand" href="#"> CAREER </a>
+          <a className="navbar-brand" href="#"> CONTACT US </a>
         </div>
     </nav>
-
-
 
 </>
     )
